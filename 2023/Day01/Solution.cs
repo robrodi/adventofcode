011d767b7921f6b6656d48607bffe8c3be62a92e
Rob Rodi <rrodi@riotgames.com>
2023-12-01 00:22:54 -0800

    cleanup 2

--- 2023/Day01/Solution.cs
+++ 2023/Day01/Solution.cs
@@ -1,13 +1,10 @@
 using System.Collections.Generic;
 using System.Linq;
-using System.Text.RegularExpressions;
 using AngleSharp.Text;
 
 namespace AdventOfCode.Y2023.Day01;
-
 [ProblemName("Trebuchet?!")]
 class Solution : Solver {
-    Regex regex = new Regex("([\\d])", RegexOptions.Compiled);
     Dictionary<string, int> numbers = new Dictionary<string, int>{
         { "one", 1},
         { "two", 2},
@@ -21,63 +18,40 @@ class Solution : Solver {
         { "zero", 0},
     };
     public object PartOne(string input) {
-        var result = input.Split(System.Environment.NewLine)
-             .Select(s => regex.Matches(s))
-             .Select(r => r.First().Groups[1].Value + r.Last().Groups[1].Value)
-             .Select(int.Parse)
-             .Sum();
-        return result;
+          return input.Split(System.Environment.NewLine)
+                      .Select(l => getInt(l, numbers, false))
+                      .Sum();
     }
 
     public object PartTwo(string input)
     {
-        var test = @"two1nine
-eightwothree
-abcone2threexyz
-xtwone3four
-4nineeightseven2
-zoneight234
-7pqrstsixteen";
-        // Console.WriteLine($"eightwothree -> {getInt("eightwothree")}. should be 83");
-        // Console.WriteLine($"abcone2threexyz -> {getInt("abcone2threexyz")}. should be 13");
-        // Console.WriteLine($"xtwone3four -> {getInt("xtwone3four")}. should be 24");
-        // Console.WriteLine($"4nineeightseven2 -> {getInt("4nineeightseven2")}. should be 42");
-        // Console.WriteLine($"zoneight234 -> {getInt("zoneight234")}. should be 14");
-        // Console.WriteLine($"7pqrstsixteen -> {getInt("7pqrstsixteen")}. should be 76");
-        
         return input.Split(System.Environment.NewLine)
-                          .Select(l => getInt(l, true))
-                          .Sum();
+                    .Select(l => getInt(l, numbers, true))
+                    .Sum();
     }
 
-    private int getInt(string line, bool useWords = true)
+    private static int getInt(string line, Dictionary<string, int> numbers, bool useWords = true)
     {
         int? first = null;
-        for (var i = 0; i < line.Length; i++)
-        {
-            first = FindFirstNumberInSubstring(line, numbers, i, useWords);
+        for (var i = 0; i < line.Length; i++){
+            first = FindFirstNumberInSubstring(line[i..], numbers, useWords);
             if (first.HasValue) break;
         }
 
         int? last = null;
-        for (var i = line.Length - 1; i >= 0; i--)
-        {
-            last = FindFirstNumberInSubstring(line, numbers, i, useWords);
+        for (var i = line.Length - 1; i >= 0; i--){
+            last = FindFirstNumberInSubstring(line[i..], numbers, useWords);
             if (last.HasValue) break;
         }
         return first.Value * 10 + last.Value;
     }
-
-    private static int? FindFirstNumberInSubstring(string line, Dictionary<string, int> numbers, int i, bool useWords = true)
+    private static int? FindFirstNumberInSubstring(string line, Dictionary<string, int> numbers, bool useWords = true)
     {
-        if (line[i].IsDigit())
-            return line[i] - '0';
-
+        if (line[0].IsDigit()) return line[0] - '0';
         if (!useWords) return null;
 
-        foreach (var number in numbers)
-        {
-            if (line.Substring(i).StartsWith(number.Key))
+        foreach (var number in numbers){
+            if (line.StartsWith(number.Key))
                 return number.Value;
         }
 
