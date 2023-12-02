b770cf8926c21ccdc42f631ca34640b0cf51ca90
Rob Rodi <rrodi@riotgames.com>
2023-12-02 08:02:06 -0800

    temp

--- src/2020/Day04/Solution.cs
+++ src/2020/Day04/Solution.cs
@@ -1,39 +1,34 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text.RegularExpressions;
 
 namespace AdventOfCode.Y2020.Day04;
 
 [ProblemName("Passport Processing")]
 class Solution : Solver {
 
-    public object PartOne(string input) => ValidCount(input, cred => 
-        rxs.All(kvp => cred.ContainsKey(kvp.Key))
-    );
+    public object PartOne(string input) => input.Split("\n\n")
+                                                .Select(Fields)
+                                                .Count(ValidatePassportFieldsPresent);
     
-    public object PartTwo(string input) => ValidCount(input, cred => 
-        rxs.All(kvp =>
-            cred.TryGetValue(kvp.Key, out var value) && Regex.IsMatch(value, "^(" + kvp.Value + ")$")
-        )
-    );
+    public static IDictionary<string, string> Fields(string input) => input.Split(new[] {'\n', ' '})
+                                                                           .Select(s => s.Split(':'))
+                                                                           .ToDictionary(s=> s[0], s=>s[1]);
+    public static bool ValidatePassportFieldsPresent(IDictionary<string, string> fields)
+    {
+        var required = new [] {"byr", "byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"}; // cid
+        return required.All(fields.ContainsKey);
+    }
+    public static bool ValidatePassportFields(IDictionary<string, string> fields){
+        bool inRange(string key, int min, int max ) => int.Parse(fields[key]) >= min && int.Parse(fields[key]) <= max;
 
-    Dictionary<string, string> rxs = new Dictionary<string, string>(){
-        {"byr", "19[2-9][0-9]|200[0-2]"},
-        {"iyr", "201[0-9]|2020"},
-        {"eyr", "202[0-9]|2030"},
-        {"hgt", "1[5-8][0-9]cm|19[0-3]cm|59in|6[0-9]in|7[0-6]in"},
-        {"hcl", "#[0-9a-f]{6}"},
-        {"ecl", "amb|blu|brn|gry|grn|hzl|oth"},
-        {"pid", "[0-9]{9}"},
-    };
+        var years = inRange("byr", 1920, 2002) && inRange("iyr", 2010,2020) && inRange("eyr", 2020, 2030);
 
-    int ValidCount(string input, Func<Dictionary<string, string>, bool> isValid) =>
-        input
-            .Split("\n\n")
-            .Select(block => block
-                .Split("\n ".ToCharArray())
-                .Select(part => part.Split(":"))
-                .ToDictionary(parts => parts[0], parts => parts[1]))
-            .Count(isValid);
+        return years;
+    }
+
+    public object PartTwo(string input) => input.Split("\n\n")
+                                                .Select(Fields)
+                                                .Where(ValidatePassportFieldsPresent)
+                                                .Count(ValidatePassportFields);
 }
