05c1b442662ef181b3c37728f80648207722598b
Rob Rodi <rrodi@riotgames.com>
2023-12-02 09:03:21 -0800

    2020.4 done

--- src/2020/Day04/Solution.cs
+++ src/2020/Day04/Solution.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace AdventOfCode.Y2020.Day04;
 
@@ -13,18 +14,32 @@ class Solution : Solver {
     
     public static IDictionary<string, string> Fields(string input) => input.Split(new[] {'\n', ' '})
                                                                            .Select(s => s.Split(':'))
+                                                                           .Where(s => s.Length == 2)
                                                                            .ToDictionary(s=> s[0], s=>s[1]);
     public static bool ValidatePassportFieldsPresent(IDictionary<string, string> fields)
     {
         var required = new [] {"byr", "byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"}; // cid
         return required.All(fields.ContainsKey);
     }
+    static Regex hr = new Regex("^(\\d+)(in|cm)", RegexOptions.Compiled);
+    static Regex passportIdR = new Regex("^[\\d]{9}$", RegexOptions.Compiled);
+    static Regex hairR = new Regex("^#[0-9a-f]{6}$", RegexOptions.Compiled);
     public static bool ValidatePassportFields(IDictionary<string, string> fields){
-        bool inRange(string key, int min, int max ) => int.Parse(fields[key]) >= min && int.Parse(fields[key]) <= max;
-
-        var years = inRange("byr", 1920, 2002) && inRange("iyr", 2010,2020) && inRange("eyr", 2020, 2030);
-
-        return years;
+        if (int.Parse(fields["iyr"]) > 2020|| int.Parse(fields["iyr"]) < 2010)  return false;
+        if (int.Parse(fields["byr"]) > 2002|| int.Parse(fields["byr"]) < 1920) return false;
+        if (int.Parse(fields["eyr"]) > 2030|| int.Parse(fields["eyr"]) < 2020) return false;
+        var hm = hr.Match(fields["hgt"]);
+        if (!hm.Success) return false;
+        if (
+            (hm.Groups[2].Value == "in" && (int.Parse(hm.Groups[1].Value) < 59 || int.Parse(hm.Groups[1].Value) > 76)) ||
+            (hm.Groups[2].Value == "cm" && (int.Parse(hm.Groups[1].Value) < 150 || int.Parse(hm.Groups[1].Value) > 193))
+        )
+           return false;
+        if (!hairR.Match(fields["hcl"]).Success) return false;
+        var ecls = new[] {"amb", "blu", "brn", "gry", "grn", "hzl", "oth"};
+        if (!ecls.Contains(fields["ecl"])) return false;
+        if (!passportIdR.Match(fields["pid"]).Success)  return false;
+        return true;
     }
 
     public object PartTwo(string input) => input.Split("\n\n")
---
+++ tests/2020/Day04/Tests.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Text;
+using Xunit;
+using AdventOfCode.Y2020.Day04;
+
+namespace AdventOfCode.tests.Y2020.Day04;
+
+[ProblemName("Passport Processing")]
+public class Tests {
+    [Fact]
+    public void Parser(){
+      var sampleInput = @"ecl:gry pid:860033327 eyr:2020 hcl:#fffffd
+byr:1937 iyr:2017 cid:147 hgt:183cm
+
+iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884
+hcl:#cfa07d byr:1929
+
+hcl:#ae17e1 iyr:2013
+eyr:2024
+ecl:brn pid:760753108 byr:1931
+hgt:179cm
+
+hcl:#cfa07d eyr:2025 pid:166559648
+iyr:2011 ecl:brn hgt:59in";
+    }
+
+    [Fact]
+    public void PartOneSample() {
+      var sampleInput = @"ecl:gry pid:860033327 eyr:2020 hcl:#fffffd
+byr:1937 iyr:2017 cid:147 hgt:183cm
+
+iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884
+hcl:#cfa07d byr:1929
+
+hcl:#ae17e1 iyr:2013
+eyr:2024
+ecl:brn pid:760753108 byr:1931
+hgt:179cm
+
+hcl:#cfa07d eyr:2025 pid:166559648
+iyr:2011 ecl:brn hgt:59in";
+    }
+
+    [Fact]
+    public void PartTwoSample() {
+      var sampleInput = @"byr valid:   2002
+byr invalid: 2003
+
+hgt valid:   60in
+hgt valid:   190cm
+hgt invalid: 190in
+hgt invalid: 190
+
+hcl valid:   #123abc
+hcl invalid: #123abz
+hcl invalid: 123abc
+
+ecl valid:   brn
+ecl invalid: wat
+
+pid valid:   000000001
+pid invalid: 0123456789";
+    }
+}
